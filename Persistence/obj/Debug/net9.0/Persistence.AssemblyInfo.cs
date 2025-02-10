f7dbebdc98a530869a1dbca9d0ec4052d7b190b7
Tyler Baxter <tbaxter@singular.co.za>
2025-02-10 15:39:11 +0200

    Seection 3

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188f69af097bc10689b80038b8cf471893a8b9dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
