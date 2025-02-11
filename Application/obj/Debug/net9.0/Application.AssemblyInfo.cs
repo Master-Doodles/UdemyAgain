17ce81fbd1a76a4fec36d389dbdb35ca65cc20f9
Tyler Baxter <tbaxter@singular.co.za>
2025-02-11 14:27:14 +0200

    Finished Section 5

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188f69af097bc10689b80038b8cf471893a8b9dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7dbebdc98a530869a1dbca9d0ec4052d7b190b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
